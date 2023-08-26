5448300a5eb3c46d1de935725d465db81c961334
Michael Eggers <mseggers@protonmail.ch>
2023-08-26 23:27:30 +0200

    IQM: Load joints and compute global and inv-global joint matrices

--- msvc.bat
+++ msvc.bat
@@ -6,5 +6,5 @@ REM  %windir%\system32\cmd.exe /k w:\handmade\misc\shell.bat
 REM
 
 
-call "G:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 cd "%~dp0"
